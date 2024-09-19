ce4adcf5184192d796d5261dc66fbd8c40de2b2a
Matheus DEV <matheusdev@L-LLQPKP2GFG.local>
2024-09-19 23:28:01 +0100

    Feat(customRace): Add test

---
+++ src/mock/RaceMock.ts
@@ -0,0 +1,9 @@
+import { customRaceModalType } from '@/controllers/createCustomRaceController'
+
+export const angelRaceInputMock: customRaceModalType = {
+  nameInput: 'Anjos',
+  sizeInput: 'Medium',
+  ageInput: 'Immortal',
+  descriptionInput:
+    'Seres com imortalidade referente a morte natural. Eles podem ser mortos em combate.',
+}
--- src/useCases/customRaces/createCustomRaceUseCase.spec.ts
+++ src/useCases/customRaces/createCustomRaceUseCase.spec.ts
@@ -1,72 +1,18 @@
 import { describe, it, expect } from 'vitest'
 import { CreateCustomRaceUseCase } from './createCustomRaceUseCase'
-import { Prisma, Size, AbilityScoreType } from '@prisma/client'
 import { InMemoryCustomRaceRepository } from '@/repositories/inMemory/inMemoryCustomRaceRepository'
-import { angelRaceMock } from '@/mock/RaceMock'
+import { angelRaceInputMock } from '@/mock/RaceMock'
 
 describe('CreateCustomRaceUseCase', () => {
   it('should create a custom race successfully', async () => {
-    const mockCustomRace: Prisma.CustomRaceCreateInput = {
-      name: angelRaceMock.raceName,
-      description: 'any description',
-      languages: angelRaceMock.languaguesInput,
-      size: Size[
-        angelRaceMock.physicalAtributesInput.size as keyof typeof Size
-      ],
-      movementSpeed: Number(angelRaceMock.physicalAtributesInput.movementSpeed),
-      traits: {
-        create: angelRaceMock.traitsInput.map((trait) => ({
-          name: trait.name,
-          description: trait.description,
-        })),
-      },
-      abilityScore: {
-        create: Object.keys(angelRaceMock.abilityScoreInput).map(
-          (abilityScoreKey) => {
-            let abilityScoreType: AbilityScoreType
-            switch (abilityScoreKey) {
-              case 'strength':
-                abilityScoreType = AbilityScoreType.STR
-                break
-              case 'dexterity':
-                abilityScoreType = AbilityScoreType.DEX
-                break
-              case 'constitution':
-                abilityScoreType = AbilityScoreType.CON
-                break
-              case 'intelligence':
-                abilityScoreType = AbilityScoreType.INT
-                break
-              case 'wisdom':
-                abilityScoreType = AbilityScoreType.WIS
-                break
-              case 'charisma':
-                abilityScoreType = AbilityScoreType.CHA
-                break
-              default:
-                abilityScoreType = AbilityScoreType.STR
-            }
-            return {
-              type: abilityScoreType,
-              value:
-                angelRaceMock.abilityScoreInput[
-                  abilityScoreKey as keyof typeof angelRaceMock.abilityScoreInput
-                ],
-            }
-          },
-        ),
-      },
-      proficienciesNumber: 0,
-      proficiencies: [],
-    }
-
+    const mockCustomRace = angelRaceInputMock
     const customRaceRepository = new InMemoryCustomRaceRepository()
 
     const createCustomRaceUseCase = new CreateCustomRaceUseCase(
       customRaceRepository,
     )
-    const result = await createCustomRaceUseCase.execute(mockCustomRace)
+    const result = await createCustomRaceUseCase.execute(mockCustomRace, '1')
 
-    expect(result.name).toEqual(mockCustomRace.name)
+    expect(result.name).toEqual(mockCustomRace.nameInput)
   })
 })
