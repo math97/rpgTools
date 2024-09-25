eb1c295f2915bca7fc2478d632589b8f15857647
Matheus DEV <matheusdev@L-LLQPKP2GFG.local>
2024-09-25 20:27:33 +0100

    Revert "chore(githubActions): setup wrangler/github and env names"
    This reverts commit 3beee7cfafde4948d549ed2714a25c8ae8044225.

--- src/utils/deployCommands.ts
+++ src/utils/deployCommands.ts
@@ -2,29 +2,26 @@ import { REST, Routes, Guild } from 'discord.js'
 import { Commands } from '@/discord/commands'
 
 export async function deployCommands(guild: Guild) {
-  const { DISCORD_CLIENT_ID, DISCORD_TOKEN } = process.env
+  const { CLIENT_ID, TOKEN } = process.env
 
   const commandsData = Object.values(Commands.list).map(
     (command) => command.data,
   )
 
-  if (!DISCORD_TOKEN) throw new Error('missing token')
+  if (!TOKEN) throw new Error('missing token')
 
-  const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN)
+  const rest = new REST({ version: '10' }).setToken(TOKEN)
   try {
-    if (!DISCORD_CLIENT_ID) throw new Error('missing token')
+    if (!CLIENT_ID) throw new Error('missing token')
     if (!guild) throw new Error('missing guild')
 
     console.log(
       ` Started refreshing application (/) commands for ${guild.name}`,
     )
 
-    await rest.put(
-      Routes.applicationGuildCommands(DISCORD_CLIENT_ID, guild.id),
-      {
-        body: commandsData,
-      },
-    )
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, guild.id), {
+      body: commandsData,
+    })
 
     console.log('Successfully reloaded application (/) commands.')
   } catch (error) {
